4a59e989c97027ca437f146cda95237776223331
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2022-05-30 11:44:08 +0300

    -Added a Test class for Yandex testing

--- SeleniumTrainingCenter/BaseTestClass.cs
+++ SeleniumTrainingCenter/BaseTestClass.cs
@@ -5,26 +5,23 @@ using OpenQA.Selenium.Chrome;
 
 namespace SeleniumTrainingCenter
 {
-    public class Tests
+    [TestClass]
+    public class BaseTestClass
     {
-        [TestClass]
-        public class EmailTestBase
+        protected const string _chromeDriverPath = @"C:\Users\AndriusBogda\Desktop\chromedriver_win32 (1)";
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void Initialize()
         {
-            protected const string _chromeDriverPath = @"C:\Users\AndriusBogda\Desktop\chromedriver_win32 (1)";
-            protected IWebDriver _driver;
+            _driver = new ChromeDriver(_chromeDriverPath);
+        }
 
-            [TestInitialize]
-            public void Initialize()
-            {
-                _driver = new ChromeDriver(_chromeDriverPath);
-            }
-
-            [TestCleanup]
-            public void CleanUp()
-            {
-                _driver.Close();
-                _driver.Dispose();
-            }
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Close();
+            _driver.Dispose();
         }
     }
 }
---
+++ SeleniumTrainingCenter/YandexTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using SeleniumTrainingCenter.Pages;
+
+namespace SeleniumTrainingCenter
+{
+    [TestClass]
+    public class YandexTest : BaseTestClass
+    {
+        private string _login = "SeleniumTestAndrius";
+        private string _password = "Qwe123rty456!";
+
+        private async Task<NewPage> GetLoggedInYandexPage(string login, string password)
+        {
+            string YANDEX_URL = @"https://yandex.lt/";
+
+            string LOGIN_BUTTON = "//*[@class='b-inline']";
+            string LOGIN_TEXTFIELD = "//input[@id='passp-field-login']";
+            string PASSWORD_TEXTFIELD = "//input[@id='passp-field-passwd']";
+            string NEXT_BUTTON = "//button[@id='passp:sign-in']";
+
+            var yandexPage = new NewPage(base._driver, YANDEX_URL);
+
+            yandexPage.ClickElementByXPath(LOGIN_BUTTON);
+
+            yandexPage.SendKeysByXPath(LOGIN_TEXTFIELD, login);
+
+            yandexPage.ClickElementByXPath(NEXT_BUTTON);
+
+            yandexPage.SendKeysByXPath(PASSWORD_TEXTFIELD, password);
+
+            yandexPage.ClickElementByXPath(NEXT_BUTTON);
+
+            return yandexPage;
+        }
+
+        [Test]
+        public async Task TestYandexLogin()
+        {
+            string USERNAME_ELEMENT = "//*[@class='username']";
+
+            var yandexPage = await GetLoggedInYandexPage(_login, _password);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("SeleniumTestAndrius", yandexPage.GetElementTextByXPath(USERNAME_ELEMENT));
+        }
+    }
+}
