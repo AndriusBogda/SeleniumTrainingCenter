f935e4299d25df97acfd95550491a04e163ae1a0
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2022-05-30 09:51:40 +0300

    -Added Pages classes

---
+++ SeleniumTrainingCenter/Pages/BasePage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumTrainingCenter.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver _driver;
+
+        public BasePage(IWebDriver driver) => _driver = driver;
+    }
+}
---
+++ SeleniumTrainingCenter/Pages/NewPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTrainingCenter.Pages
+{
+    public class NewPage : BasePage
+    {
+        public IWebDriver Driver
+        {
+            get => base._driver;
+        }
+        public string Url
+        {
+            get => base._driver.Url;
+            set => base._driver.Navigate().GoToUrl(value);
+        }
+
+        private IWebElement GetElementByXPath(string xPath)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 10));
+
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(xPath)));
+        }
+
+        public void SendKeysByXPath(string xPath, string content)
+        {
+            GetElementByXPath(xPath).SendKeys(content);
+        }
+
+        public void ClickElementByXPath(string xPath)
+        {
+            GetElementByXPath(xPath).Click();
+        }
+
+        public string GetElementTextByXPath(string xPath)
+        {
+            return GetElementByXPath(xPath).Text;
+        }
+
+        public NewPage(IWebDriver driver, string url) : base(driver)
+        {
+            Url = url;
+        }
+    }
+}
