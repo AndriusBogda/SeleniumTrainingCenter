599be1322a3d3a7424bf16f2fa7cc36f9581492c
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2022-06-02 14:08:56 +0300

    - Added entities

---
+++ SeleniumTrainingCenter/Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTrainingCenter.Entities
+{
+    public class Employee : Person
+    {
+        public string Postion { get; set; }
+        public string Office { get; set; }
+        public Employee(string name, string postion, string office) : base(name)
+        {
+            Postion = postion;
+            Office = office;
+        }
+    }
+}
